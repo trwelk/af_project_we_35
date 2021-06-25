39cdfe74acffc201cd3bda6d6bb35bea392bc191
trwelk <weerasooriya.trewon@gmail.com>
2021-06-26 03:22:11 +0530

    Added Charts for Workshop

--- frontend/src/pages/WorkshopDisp.js
+++ frontend/src/pages/WorkshopDisp.js
@@ -11,6 +11,9 @@ import ImageTileDisplay from '../components/views/util/ImageTileDisplay';
 import KeynoteCard from '../components/keynote/KeynoteCard';
 import KeynoteList from '../components/keynote/KeynoteList';
 import NewsList from '../components/news/NewsList';
+import { Parallax } from 'react-parallax';
+import Heading1 from '../components/views/util/Heading1';
+import ParagraphWithBackground from '../components/views/util/ParagraphWithBackground';
 
 const useStyles = makeStyles((theme) => ({
     listContainer: {
@@ -40,13 +43,13 @@ function WorkshopDisp() {
         )
 
     })
+    const intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 
     return (
-        <div>
-            <CategoryChips />
-            <div className={classes.listContainer} >
-                {workShopUiList}
-            </div>
+        <div style={{background:"#fff5f8"}}>
+            <Heading1 data={{heading:"BRAINYCONN"}}/>
+            <ParagraphWithBackground text={intro}/>
+            
             <ImageTileDisplay/>
             <KeynoteList/>
             <NewsList/>
