699146bcc2349ba3236b35fef3eab3be38f8ee63
trwelk <weerasooriya.trewon@gmail.com>
2021-06-30 03:43:00 +0530

    Add validation method

--- frontend/src/pages/News.js
+++ frontend/src/pages/News.js
@@ -2,16 +2,21 @@ import React from 'react';
 import NewsList from '../components/news/NewsList';
 import UserTable from '../components/user/UserTable'
 import Heading1 from '../components/views/util/Heading1';
+import NavBar from '../components/views/util/NavBar';
 import ParagraphWithBackground from '../components/views/util/ParagraphWithBackground';
 function News() {
 
   const paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   return (
-    <div style={{background:"#fff5f8"}}>
+    <div>
+      <NavBar/>
+      <div style={{background:"#fff5f8",padding: "20px",marginTop:"70px"}}>
         <Heading1 data={{heading:"Stay Updated"}} />
         <ParagraphWithBackground text={paragraph}/>
         <NewsList/>
       </div>
+    </div>
+    
   );
 }
 
