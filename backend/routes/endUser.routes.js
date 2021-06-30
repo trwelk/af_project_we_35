9e8ca5a8b48d1ef3a17029f8dd56267e1e063909
IT19163864 <64912486+ryanmlk@users.noreply.github.com>
2021-06-30 17:11:54 +0530

    Removed enduser login

--- backend/routes/endUser.routes.js
+++ backend/routes/endUser.routes.js
@@ -5,28 +5,7 @@ const Router = require('@koa/router');
 const router = new Router({
     prefix: '/enduser'
    });
-   //Used for a user to login. The users details along with a json web token is also sent back for authentication
-   router.post('/login',async ctx => {
-    let user = ctx.request.body;
-    user = await EndUserApi.userLogin(user.email, user.password);
-    let token = {}
-    if(user){
-             token = jsonwebtoken.sign({
-                id: user.id,
-                email: user.email,
-                type: user.type,
-                contact: user.contact,
-                name: user.name,
-                username: user.username,
-                institution: user.institution
-            }, "jwtSecret")
-            ctx.body = {user:user,token:token};
-            ctx.response.status = 201;
-        }
-        else{
-            ctx.response.body = "AUTHERROR";
-        }
-   });
+   
    //Used for a user to sign up
    router.post('/', async ctx => {
     let user = ctx.request.body;
